5ee9e29df2146e5ae421acb2a7a402d0fe1b9fa7
gilara01 <37631828+gilara01@users.noreply.github.com>
2018-07-11 18:08:17 -0300

    Update P5.c

--- P5.c
+++ P5.c
@@ -1,54 +1,45 @@
 //Giovanna Lara Cabrera Gaspar 11721ECP008
 #include <stdio.h>
-
-int main()
-{	char str[256];
-	char *p;
-	int i, txt[256], opcao;
-	int *ps;
-
-	do{printf(" \n1 - Codificar \n2 - Decodificar \n3 - Sair do programa  ");
-	scanf("%d", &opcao);
-	getchar(); 
-	
-	switch(opcao)
-	{
-			case 1:
-			scanf("%s",str);
-		
-		int *ps;
-		
-		for(i=0; str[i]!='\0'; i++)
-		
-		{
-			ps = (int*) &str[i];
-			if(str[i]!='\0')
-				printf("%d, ", *ps);
-				
-			else
-				printf("%d", *ps);		
-				
-		}
-			
-		case 2:
-			int num[64];
-		char dec;
-		
-		for(i=0; i<64; i++)
-		{
-			scanf("%d%c", &num[i], &dec);
-			if(dec!=',')
-				break;
-		}
-		
-		printf("%s", &num);
-	
-		 	
-
-}
-
-
-}
- while  (opcao!=3);
-
+int main(){
+    int i=0,opcao,num[64];
+    char str[255];
+    int pi = (int)str;
+    char ps = (char)&num;
+    do{printf("1 - Codificar\n2 - Decodificar\n3 - Sair do programa\nOpcao: ");
+    scanf("%d",&opcao);
+    getchar(); 
+    switch(opcao){
+        case 1:
+               printf("Insira a frase: ");
+            fgets(str,255,stdin);
+            for(i=0; str[i]!='\0'; i++){
+                if(str[i]==10){
+                    str[i]==0;
+                }
+            }
+            while(pi!=0){
+                printf("\nResultado obtido: %d",pi);
+                pi++;
+            }
+            printf("\n\n");
+            break;
+        case 2:
+            for(i=0; i<64; i++){
+                scanf("%d",&num[i]);
+                if(getchar()=='\n'){
+                    break;
+                }
+            }
+            printf("\nResultado Obtido: ");
+            while(ps!=0){
+                printf("%c",ps);
+                ps++;
+            }
+            printf("\n\n");
+            break;
+    }
+    }while(opcao!=3);
+    if(opcao<0 || opcao>2){
+        return 0;
+    }
 }
